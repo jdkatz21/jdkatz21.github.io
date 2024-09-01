39bc7f3a83bc37c38aa1249c539b73de5ab78891
Jared Katz <65374727+jdkatz21@users.noreply.github.com>
2024-09-01 17:51:31 -0400

    Update brownian_motion_script.js

--- dataviz/brownian_motion_script.js
+++ dataviz/brownian_motion_script.js
@@ -121,7 +121,7 @@ d3.csv('brownian_motions.csv').then(function(data) {
         for (let i = 0; i < data.length; i += step) {
             const t = i / (data.length - 1) * 4;  // Calculate the corresponding x value (0 to 2 range)
             console.log(t)
-            sampledData.push({ x: +t * .5, y: data[i] });
+            sampledData.push({ x: +t, y: data[i] });
 
         }
         
